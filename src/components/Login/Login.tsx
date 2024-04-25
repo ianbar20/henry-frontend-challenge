166410a11ce64cc76c9dd69161080d002bcbbf96
Ian Barnard <ianbar20@gmail.com>
2024-04-25 07:47:46 -0600

    fix type

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -22,7 +22,7 @@ const Login = ({ setUser, addNewProvider, addNewClient, userType }: LoginProps) 
       <form className="bg-white shadow-xl rounded p-12 mb-4" onSubmit={handleSubmit}>
         <label className="block text-2xl font-bold mb-4 text-center" htmlFor="enterId">Enter your ID</label>
         <input type="text" className="border-2" id="enterId" value={id} onChange={e => setId(e.target.value.toLowerCase())} placeholder="Enter ID" required />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold ml-2 py-2 px-4 rounded" type="Submit">Log in</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold ml-2 py-2 px-4 rounded" type="submit">Log in</button>
       </form>
     </div>
   );
